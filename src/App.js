597c89e564f01d3aaf16779e11f3a52d0973f07b
Toluwanimi <rjayjutolu419@gmail.com>
2023-03-02 12:26:36 +0100

    Added the shuffle cards feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const cardImages = [
+  { "src": "/img/helment-1.png"},
+  { "src": "/img/potion-1.png"},
+  { "src": "/img/ring-1.png"},
+  { "src": "/img/scroll-1.png"},
+  { "src": "/img/sheild-1.png"},
+  { "src": "/img/sword-1.png"}
+]
+
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const shuffleCards = () => {
+    const shuffledCards = [ ...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map(card => ({ ...card, id: Math.random()}));
+
+      setCards(shuffledCards);
+      setTurns(0);
+  }
+
+  shuffleCards();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Magic Match</h1>
+      <button onClick = {shuffleCards}>New Game</button>
     </div>
   );
 }
